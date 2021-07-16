19992c4c91fdb283b5e3eda9f15c32fae5122407
João Martinho <joao.ribeiro.martinho@gmail.com>
2021-07-16 22:13:17 +0100

    Environment variable for mongo connection string

--- cazoo-backend/app/config/db.config.js
+++ cazoo-backend/app/config/db.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  url: 'mongodb://root:toor@mongo:27017/cazoo-database?authSource=admin&authMechanism=SCRAM-SHA-1'
+  url: process.env.DB_URI
+    ? process.env.DB_URI
+    : 'mongodb://root:toor@localhost:27017/cazoo-database?authSource=admin&authMechanism=SCRAM-SHA-1'
 };
